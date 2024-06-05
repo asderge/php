6f39db97b0d0b2354862bb3227489df7e568fe90
asderge <tangsj719@gmail.com>
2024-06-05 09:04:25 +0800

    Create 14.user_add_form.php

---
+++ 14.user_add_form.php
@@ -0,0 +1,28 @@
+<html>
+    <head><title>新增使用者</title></head>
+    <body>
+<?php
+    // 設定錯誤報告為不顯示任何錯誤
+    error_reporting(0);
+    
+    // 開始 session
+    session_start();
+    
+    // 檢查是否已登入，如果未登入，顯示提示訊息並重新導向至登入頁面
+    if (!$_SESSION["id"]) {
+        echo "請登入帳號";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else {
+        // 如果已登入，顯示新增使用者的表單
+        echo "
+            <form action=15.user_add.php method=post>
+                帳號：<input type=text name=id><br>
+                密碼：<input type=text name=pwd><p></p>
+                <input type=submit value=新增> <input type=reset value=清除>
+            </form>
+        ";
+    }
+?>
+    </body>
+</html>
