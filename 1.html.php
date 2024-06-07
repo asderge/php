95022e20f45ad2d0331254e034195abea5da2cfe
asderge <tangsj719@gmail.com>
2024-06-07 09:56:30 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,19 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php 
+        // 這裡使用 PHP 印出了 "Hello world!"，這段程式碼會在網頁中顯示出來。
+        echo "Hello world!"; 
+        ?> 
+        <h1>html</h1>
+        <?php 
+        // 同樣，這裡也使用 PHP 印出了 "Hello world!"。
+        echo "Hello world!"; 
+        ?> 
+        <h1>html</h1>
+        <?php 
+        // 最後一次使用 PHP 印出了 "Hello world!"。
+        echo "Hello world!"; 
+        ?> 
+    </body>
+</html>
